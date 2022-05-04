95abe7fe17961f22120123d86f7cecc4c248d91b
cristianbota001 <aspire.wayy@gmail.com>
2022-05-04 19:16:11 +0200

    end form

--- rest/classes/forms.php
+++ rest/classes/forms.php
@@ -8,11 +8,18 @@
 
         function ValidateLoginForm($form){
             $errors = ["response" => []];
+
             if (!$form["username"]){
                 $errors["response"]["username"] = "Compilare il campo";
             }
+            else if (!preg_match('/^[a-zA-Z0-9]+$/', $form["username"]) == 1){
+                $errors["response"] = ["username" => "Caratteri non validi"];
+            }
             if (!$form["password1"]){
                 $errors["response"]["password1"] = "Compilare il campo";
+            }
+            else if (strlen($form["password1"]) < 8){
+                $errors["response"]["password1"] = "Inserire minimo 8 caratteri";
             }
             if (!$errors["response"]){
                 if (!$this->dbaccess->Authenticate($form)){
@@ -30,10 +37,12 @@
                     if ($this->dbaccess->CheckIfUserExists($form["username"])){
                         $errors["response"] = ["username" => "Username gia esistente"];
                     }
-                }else{
+                }
+                else{
                     $errors["response"] = ["username" => "Caratteri non validi"];
                 }
-            }else{
+            }
+            else{
                 $errors["response"]["username"] = "Compilare il campo";
             }
             if (!$form["password1"]){
@@ -45,6 +54,10 @@
             if ($form["password1"] && $form["password2"]){
                 if ($form["password1"] != $form["password2"]){
                     $errors["response"]["password2"] = "Riscrivere correttamente la password";
+                }
+                else if (strlen($form["password1"]) < 8){
+                    $errors["response"]["password1"] = "Inserire minimo 8 caratteri";
+                    $errors["response"]["password2"] = "Inserire minimo 8 caratteri";
                 }
             }
             if (!$errors){
