8bf6dd72527c5eda6ec64f7c998016985b1bf13b
cristianbota001 <aspire.wayy@gmail.com>
2022-05-09 19:01:46 +0200

    improve

---
+++ client/views/home/COMPONENTS/comp1/JS/comp1.js
@@ -0,0 +1,22 @@
+var comp1;
+
+window.onload = () => {
+
+    comp1 = new Comp1();
+    
+}
+
+class Comp1 extends Home{
+
+    constructor(){
+        super()
+        this.AddEvents()
+    }
+
+    AddEvents(){
+        document.querySelector(".option_button").addEventListener("click", () => {
+            document.querySelector(".filter_menu_div").classList.toggle("filter_menu_div_toggle")
+        })
+    }
+
+}
